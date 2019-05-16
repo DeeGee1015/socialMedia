5dac3df8916b9666841ec304ef956f43b89b97f2
Devin Guerrero <devinguerrero1015@gmail.com>
2019-05-16 10:06:58 -0400

    okay

---
+++ main.js
@@ -0,0 +1,29 @@
+// class classic {
+//
+//   construtor(){
+//     this.frame = 'black-border'
+//
+//     this.art_url = ''
+//   }
+//
+// render() {
+// let el =document.createElement('div')
+// el.style.width
+// el.style.height
+// el.style.backgroundColor
+// document.body.appendChild(el)
+// }
+//
+// }
+const devin = document.getElementById('devin')
+
+class Classic {
+  constructor(art_url) {
+    this.frame = 'black-border'
+    this.art_url = art_url
+
+  }
+render() {
+  console.log('this isnt working')
+}
+}
